1c2a74f0abe1c433dc2709ac995d9cbdebc2aa85
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-02-20 13:00:11 -0300

    trabalhando com arquivos

---
+++ CSharp/CSharp_TrabalhandoComArquivos/CSharp_TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CSharp_TrabalhandoComArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Directory e DirectoryInfo
+
+            var folderName = "pasta";
+            var subFolderName = "subpasta";
+            var subFolderNameStatic = "subpasta_usingstatic";
+
+            var directoryInfoSubFolder = new DirectoryInfo(subFolderName);
+
+            if (!directoryInfoSubFolder.Exists(subFolderName) || !directoryInfoSubFolder.Exists)
+            {
+                Directory.CreateDirectory(subFolderNameStatic);
+                Directory.CreateDirectory(folderName);
+                directoryInfoSubFolder.Create();
+
+                directoryInfoSubFolder.MoveTo($"{folderName}//{subFolderName}");
+                Directory.Move(subFolderNameStatic, $@"{folderName}\{subFolderNameStatic}");
+            }
+
+            var name = directoryInfoSubFolder.Name;
+            var parent = directoryInfoSubFolder.Parente;
+            var root = directoryInfoSubFolder.Root;
+            var exists = directoryInfoSubFolder.Exists;
+
+            foreach (var directory in Directory.GetDirectories(folderName))
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Delete($@"{folderName}\{subFolderName}");
+
+            #endregion
+        }
+    }
+}
