0dab3fc764116ff1bad4c4b3807e7b9189aa14bc
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-02-21 10:37:43 -0300

    commit debugging

---
+++ CSharp/CSharp_DebugandoAplicacoes/CSharp_DebugandoAplicacoes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharp_DebugandoAplicacoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Debugging
+
+            var numerosString = Console.ReadLine();
+            var numeros = numerosString.Split(' '); // Se estiver errada, você pode testar mudar o valor e avançar com código
+            // o split transforma os número em arrays ou matrizes de números com espaços entre elas
+
+            foreach (var numero in numeros) //laço de repetição
+            {
+                var numeroInt = int.Parse(numero); // conerte um a um em inteiro
+
+                var aoQuadrado = Math.Pow(numeroInt, 2);
+
+                Console.WriteLine($"{numeroInt}² = {aoQuadrado}");
+            }
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
