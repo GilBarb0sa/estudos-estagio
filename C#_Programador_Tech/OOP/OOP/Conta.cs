b395ec1e200e2f5f31061ef8fb02542bd11123fb
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-02-23 10:14:34 -0300

    commit curso c#
    Inicio do Curso do Programador Tech

---
+++ C#_Programador_Tech/OOP/OOP/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Conta 
+    {
+        public double Saldo { get; set; } // Get : Diz que pode ser acessado, pode ser buscado - Set : Pode adicionar um valor para este saldo
+        public double Limite { get; set; }
+        public int Numero { get; set; }
+    }
+}
---
+++ C#_Programador_Tech/OOP/OOP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo OOP!");
+
+            Conta conta1 = new Conta(); // Para criar um objeto, você precisa criar uma instância da class Conta
+
+            Conta conta2 = new Conta(); // Nova instância
+
+            //Atribuindo os valores para conta 1
+            conta1.Saldo = 1500;
+            conta1.Limite = 500;
+            conta1.Numero = 123;
+
+            //Atribuindo os valores para conta 2
+            conta2.Saldo = 2000;
+            conta2.Limite = 600;
+            conta2.Numero = 1456;
+
+            Console.WriteLine("Olá, fulano, seu saldo em conta é: " + conta1.Saldo );
+            Console.WriteLine("Limite de saque: " + conta1.Limite);
+            Console.WriteLine("O numero da conta é: " + conta1.Numero);
+
+            Console.WriteLine("--------------------------------------------------------------");
+
+            Console.WriteLine("Olá, fulano, seu saldo em conta é: " + conta2.Saldo);
+            Console.WriteLine("Limite de saque: " + conta2.Limite);
+            Console.WriteLine("O numero da conta é: " + conta2.Numero);
+
+
+        }
+    }
+}
