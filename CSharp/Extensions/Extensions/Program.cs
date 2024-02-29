7c58291346005df33199fa9124a575f1f3eca655
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-02-29 12:24:17 -0300

    como funciona as extensoes

---
+++ CSharp/Extensions/Extensions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 10;
+
+            Console.WriteLine(numero.EhMaior(10));
+
+            Console.WriteLine(numero.MultiplicaPor100());
+
+            var conta = new Conta();
+            conta.PagarConta();
+            conta.PagarContaComCartao();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSharp/Extensions/Extensions/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class Utils
+    {
+        public static bool EhMaior(this int param, int secondParam)
+        {
+            return param >= secondParam;
+        }
+
+        public static int MultiplicaPor100(this int param)
+        {
+            return param * 100;
+        }
+
+        public static void PagarContaComCartao(this Conta conta)
+        {
+            Console.WriteLine("Pagando a conta com cartão");
+        }
+    }
+}
