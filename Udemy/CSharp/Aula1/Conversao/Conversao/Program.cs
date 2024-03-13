4a8331492beb8a0923521d636afa4ac70ee4e412
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-03-13 12:48:57 -0300

    commit conversao

--- Udemy/CSharp/Aula1/Conversao/Conversao/Program.cs
+++ Udemy/CSharp/Aula1/Conversao/Conversao/Program.cs
@@ -6,18 +6,55 @@ namespace Conversao
     {
         static void Main(string[] args)
         {
-            byte num1 = 100; // 8bits, de 0 a 255
-            ushort nume2; // 16bits, de 0 a 65.535
+            #region Conversão Implícita
+            //byte num1 = 100; // 8bits, de 0 a 255
+            //ushort nume2; // 16bits, de 0 a 65.535
 
-            float num3 = 1250.45f;
-            double num4 = num3;
+            //float num3 = 1250.45f;
+            //double num4 = num3;
 
-            num3 = num1;
-            nume2 = num1;
+            //num3 = num1;
+            //nume2 = num1;
 
-            int numero = 'C';
+            //int numero = 'C';
+            #endregion
 
-            Console.WriteLine(numero);
+            #region Conversão Explícita
+            //ushort num1 = 500;
+            //byte num2 = (byte)num1;
+
+            //float num3 = 2500.786f;
+            //int num4 = (int)num3;
+
+            //char letra = (char)97;
+
+            #endregion
+
+            #region Método Parse
+
+            //string txtNumero = "1917";
+
+            //int numero = int.Parse(txtNumero);
+
+            //byte num1 = byte.Parse("120");
+
+            //double num2 = double.Parse("125623,57");
+
+            //float num3 = float.Parse("457,75");
+
+            #endregion
+
+            #region
+
+            string texto = Convert.ToString(2500);
+
+            double num1 = Convert.ToDouble(false);
+
+            int num2 = Convert.ToInt32('C');
+
+            #endregion
+
+            Console.WriteLine(num2);
             Console.ReadKey();
         }
     }
