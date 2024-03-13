331f4ac5bfd441be50fbca1b3b49e313ce9a2fb3
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-03-13 12:00:04 -0300

    commit conversao implicita

---
+++ Udemy/CSharp/Aula1/Conversao/Conversao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Conversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num1 = 100; // 8bits, de 0 a 255
+            ushort nume2; // 16bits, de 0 a 65.535
+
+            float num3 = 1250.45f;
+            double num4 = num3;
+
+            num3 = num1;
+            nume2 = num1;
+
+            int numero = 'C';
+
+            Console.WriteLine(numero);
+            Console.ReadKey();
+        }
+    }
+}
