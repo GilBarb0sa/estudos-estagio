dd2827bba62442f7a127f94e17b45336ae5c4058
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-03-13 11:15:45 -0300

    commit entrada e saida

---
+++ Udemy/CSharp/Aula1/EntradaSaida/EntradaSaida/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EntradaSaida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Gilberto");
+            //Console.Write("Barbosa");
+
+            //int codigo = Console.Read();
+            //Console.WriteLine(codigo);
+
+            string texto = Console.ReadLine();
+            Console.WriteLine(texto);
+
+            Console.ReadKey();
+        }
+    }
+}
