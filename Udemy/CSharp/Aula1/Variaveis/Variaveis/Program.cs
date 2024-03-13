9ec69ccda4d59e3a878512d07268f79118a0a23e
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-03-13 10:48:19 -0300

    estudos de variaveis

--- Udemy/CSharp/Aula1/Variaveis/Variaveis/Program.cs
+++ Udemy/CSharp/Aula1/Variaveis/Variaveis/Program.cs
@@ -1,13 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Principal;
 
 namespace Variaveis
 {
     internal class Program
     {
+        enum Notas
+        {
+            Minimo,
+            Media,
+            Maximo
+        }
+
+        struct Pessoa
+        {
+            public string nome;
+            public int idade;
+            public double altura;
+        }
+
         static void Main(string[] args)
         {
             #region Numericas Integrais
@@ -30,7 +41,96 @@ namespace Variaveis
             //numero = num1;
             #endregion
 
-            Console.WriteLine(numero);
+            #region Números Reais
+
+            //float real1 = 100.75f;
+            //double real2 = 12500.45;
+            //decimal real3 = 752538.457m;
+
+            //double valor;
+            //valor = real1;
+
+            #endregion
+
+            #region Caractere
+
+            //char letra = '\u0042';
+            //char escape = '\n';
+            //char literal = 'C';
+
+            #endregion
+
+            #region Boolean
+
+            //bool verificar = false;
+            //verificar = true;
+
+            #endregion
+
+            #region String (Cadeia de Caracteres)
+
+            //string texto = @"Gilberto Barbosa \n 1991";
+            //string mensagem = null;
+            //mensagem = texto;
+
+            #endregion
+
+            #region Tipo Implícito Var
+
+            //var valor = 100;  //Como foi declarado no começo o tipo do var, que no caso foi int, não é possivel alterar o tipo subsquente
+            //valor = 20;
+            //valor = "Gil";
+
+            #endregion
+
+            #region Tipo Object (Base para todos os tipos)
+
+            //object obj = false;  //O object ela pode ser alterado o seu conteudo pois ele é um objeto, não tem uma limitação como um var.
+            //obj = 200;
+            //obj = "Gilberto";
+
+            #endregion
+
+            #region Constantes
+
+            //const double pi = 3.1415;
+            //const string nome = "Gilberto";
+
+            #endregion
+
+            #region Enumeração
+
+            //Notas notasAlunos = Notas.Media;
+
+            #endregion
+
+            #region Struct
+
+            Pessoa p1 = new Pessoa();
+            p1.altura = 1.65;
+            p1.idade = 35;
+            p1.nome = "Gabriel";
+
+            Pessoa p2 = new Pessoa()
+            {
+                nome = "Gilberto",
+                idade = 32,
+                altura = 1.81
+            };
+
+            p1.nome = "Rodrigo";
+
+            Console.WriteLine(p1.nome);
+            Console.WriteLine(p1.idade);
+            Console.WriteLine(p1.altura);
+            Console.WriteLine();
+            Console.WriteLine(p2.nome);
+            Console.WriteLine(p2.idade);
+            Console.WriteLine(p2.altura);
+
+            #endregion
+
+            Console.WriteLine();
             Console.ReadKey();      
         }
     }
