ec6bc486e76662ad59f36a3fee3f7b46d425bd01
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-03-15 12:42:42 -0300

    commit operadores

--- Udemy/CSharp/Aula1/Operadores/Operadores/Program.cs
+++ Udemy/CSharp/Aula1/Operadores/Operadores/Program.cs
@@ -29,19 +29,73 @@ namespace Operadores
 
             #region Incremento e Decremento
 
-            int num1 = 10;
+            //int num1 = 10;
 
-            //num1 = num1 + 1;
-            //num1++;
-            //++num1;
-            //num1--;
-            //--num1;
+            ////num1 = num1 + 1;
+            ////num1++;
+            ////++num1;
+            ////num1--;
+            ////--num1;
 
-            Console.WriteLine(num1++);
-            Console.WriteLine(num1);
+            //Console.WriteLine(num1++);
+            //Console.WriteLine(num1);
 
             #endregion
 
+            #region Concatenação
+
+            //string nome = "Gilberto ";
+            //string sobreNome = "Barbosa";
+            //string nomeCompleto = nome + sobreNome + " " + 1991;
+            //Console.WriteLine(nomeCompleto);
+
+            #endregion
+
+            #region Atribuição
+
+            //int num1 = 13;
+            //num1 += 20;
+            //num1 -= 2;
+            //num1 *= 3;
+            //num1 /= 2;
+            //num1 %= 2;
+
+            //string nome = "Gilberto";
+            //nome += " Barbosa ";
+            //nome += 1991;
+
+            #endregion
+
+            #region Igualdade/Comparação
+
+            //bool res = 100 == (50 * 2);
+            //bool res2 = (100 / 2) != (100 + 100);
+
+            //string nome = "Gilberto";
+            //bool res3 = nome == "Lucas";
+
+            #endregion
+
+            #region Relacionais
+
+            //bool res = 100 < 50 * 2;
+            //bool res = 100 + 25 > 50 * 2;
+            //bool res = 100 <= 50 * 2;
+            //bool res = 75 >= 50;
+
+            #endregion
+
+            #region
+
+            bool res1 = 100 >= 50;
+            bool res2 = 60 == 50;
+
+            bool final = res1 && res2;
+            bool final2 = (100 >= 50) || (60 == 50);
+
+            #endregion
+
+            Console.WriteLine(final);
             Console.ReadKey();
         }
     }
