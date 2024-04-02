7aaeada339587b8aedcb18a4f64c3a36ef1f4910
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-04-02 12:08:32 -0300

    commit ultimos exemplos de poo

---
+++ Udemy/CSharp/Aula1/POO/Polimorfismo/Conta.cs
@@ -0,0 +1,35 @@
+﻿namespace Polimorfismo
+{
+    class CaixaEletronico : IConta
+    {
+        public string Usuario { get; set; }
+
+        public void Depositar()
+        {
+            
+        }
+
+        public void Sacar()
+        {
+           
+        }
+
+        public void Saldo()
+        {
+           
+        }
+
+        public void SolicitarEmprestimo()
+        {
+
+        }
+    }
+
+    interface IConta // Uma interface é abstract, por isso não é necessario colocar nos seus objetos e metodos.
+    {
+        string Usuario { get; set; }
+        void Depositar();
+        void Sacar();
+        void Saldo();
+    }
+}
---
+++ Udemy/CSharp/Aula1/POO/Polimorfismo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //Forma a = new Forma(); ao tranformar a classe abstract não é mais possivel instancia-lo mas as heranças funcionam normalmente
+            Forma b = new Triangulo();
+            Forma c = new Circulo();
+            Forma d = new Retangulo();
+
+            //Console.WriteLine("Forma");
+            //a.Desenhar();
+            Console.WriteLine("Triangulo");
+            b.Desenhar();
+            Console.WriteLine("Circulo");
+            c.Desenhar();
+            Console.WriteLine("Retangulo");
+            d.Desenhar();
+
+            Console.ReadKey();
+        }
+    }
+}
