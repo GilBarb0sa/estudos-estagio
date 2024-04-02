30050199e4761bab91a083a4a4e85810581a42b4
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-04-02 13:00:27 -0300

    commit mediageral poo

---
+++ Udemy/CSharp/Aula1/MediaGeralPOO/MediaGeralPOO/Aluno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MediaGeralPOO
+{
+    class Aluno
+    {
+        public string Nome {  get; private set; } // O nome de aluno não é possivel alterar, pois o set esta private
+        private double[] _notas;  //Os [] sinaliza que este objeto é uma array
+        public double Media
+        {
+            get
+            {
+                return CalcularMedia();
+            }
+        }
+
+        public Aluno(string nome, int provas) // construtor, inicia sempre com o nome da classe
+        {
+            Nome = nome;
+            _notas = new double[provas];
+        }
+
+        public void InserirNotas()
+        {
+            for (int i = 0; i < _notas.Length; i ++)
+            {
+                Console.Write("Notas #" + (i + 1) + ": ");
+                _notas[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        private double CalcularMedia()
+        {
+            double total = 0;
+            for (int i = 0; i < _notas.Length; i ++)
+            {
+                total += _notas[i];
+            }
+
+            return total / _notas.Length;
+        }
+    }
+}
---
+++ Udemy/CSharp/Aula1/MediaGeralPOO/MediaGeralPOO/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MediaGeralPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "### Média Geral dos Alunos ###";
+
+            Console.Write("Quanto alunos? ");
+            int nAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Aluno[] alunos = new Aluno[nAlunos];
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                Console.Clear();
+                Console.Write("Aluno #" + (i+1) + ": ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Aluno #" + (i + 1) + " Provas: ");
+                int provas = int.Parse(Console.ReadLine());
+
+                alunos[i] = new Aluno(nome, provas);
+
+                Console.WriteLine("Insira as notas do aluno " + nome);
+                alunos[i].InserirNotas();
+            }
+
+            Console.Clear();
+
+            double mediaGeral = 0;
+
+            foreach ( Aluno aluno in alunos )
+            {
+                Console.WriteLine("Aluno: " + aluno.Nome);
+                Console.WriteLine("Media: " + aluno.Media.ToString("N2"));
+                Console.WriteLine();
+
+                mediaGeral += aluno.Media;
+            }
+
+            double resultadoFinal = mediaGeral / alunos.Length;
+
+            Console.WriteLine("Média geral dos alunos: " + resultadoFinal.ToString("N2"));
+
+            Console.ReadKey();
+        }
+    }
+}
