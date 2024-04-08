3c2b54e1de5482efff2c3170fb71939d89dd749c
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-04-08 11:03:03 -0300

    metodo main

---
+++ Udemy/CSharp/Aula1/MetodoMain/MetodoMain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MetodoMain
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = args[0];
+            string senha = "abc123";
+
+            if (senha != args[1])
+            {
+                Console.WriteLine("Acesso Negado!");
+            } 
+            else
+            {
+            Console.WriteLine("Ola " + args[0]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
