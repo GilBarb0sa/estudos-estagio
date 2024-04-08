7ef2c9d04e50ba37fef8d0eb4209de3aceadb8b0
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-04-08 10:34:34 -0300

    recursiva

---
+++ Udemy/CSharp/Aula1/Recursividade/Recursividade/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Recursividade
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+           Recursiva r = new Recursiva();
+           r.ExecutarRecursivo("Ola Mundo!", 3);
+
+           Console.ReadKey();
+        }
+    }
+}
---
+++ Udemy/CSharp/Aula1/Recursividade/Recursividade/Recursiva.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recursividade
+{
+    class Recursiva
+    {
+
+        //Metodo não recursivo
+        public void Executar(string mensagem, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(mensagem);
+            }
+        }
+
+        //Metodo recursivo
+        public void ExecutarRecursivo(string mensagem, int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(mensagem);
+                ExecutarRecursivo(mensagem, n-1);
+            }
+        }
+    }
+}
