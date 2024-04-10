7f40d817d13aaf24cc78faab5ba3d0fdd1c3c662
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-04-10 13:01:35 -0300

    manipulando string parte1

---
+++ Udemy/CSharp/Aula1/WimanipularString/WimanipularString/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WimanipularString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        { 
+        #region Contains / ToString
+        //string texto = "Este tipo de variável é alfanumerica.";
+
+        //if (texto.Contains("tipo de var"))
+        //{
+        //    label1.Text = "Contém.";
+        //}
+        //else
+        //{
+        //    label1.Text = "Não Contém.";
+        //}
+
+        //int num = 2024;
+        //bool res = true;
+        //label1.Text = res.ToString();
+        #endregion
+
+        #region ToUpper / ToLower
+
+        string nome = "Gilberto";
+        string nomeMaiusculo = nome.ToUpper();
+        string nomeMinusculo = nome.ToLower();
+        //label1.Text = nomeMaiusculo;
+        label1.Text = nomeMinusculo;
+
+        #endregion
+        }
+    }
+}
