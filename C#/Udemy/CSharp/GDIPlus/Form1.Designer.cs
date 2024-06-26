31286752de137f1a28fae4b597c09953d1a59c6f
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-06-26 11:48:08 -0300

    como desenhar no csharp + gdi

---
+++ C#/Udemy/CSharp/GDIPlus/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace GDIPlus
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.picture = new System.Windows.Forms.PictureBox();
+			this.btnDesenhar = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// picture
+			// 
+			this.picture.Location = new System.Drawing.Point(13, 13);
+			this.picture.Name = "picture";
+			this.picture.Size = new System.Drawing.Size(692, 421);
+			this.picture.TabIndex = 0;
+			this.picture.TabStop = false;
+			// 
+			// btnDesenhar
+			// 
+			this.btnDesenhar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnDesenhar.Location = new System.Drawing.Point(481, 440);
+			this.btnDesenhar.Name = "btnDesenhar";
+			this.btnDesenhar.Size = new System.Drawing.Size(194, 55);
+			this.btnDesenhar.TabIndex = 1;
+			this.btnDesenhar.Text = "Desenhar";
+			this.btnDesenhar.UseVisualStyleBackColor = true;
+			this.btnDesenhar.Click += new System.EventHandler(this.btnDesenhar_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(717, 507);
+			this.Controls.Add(this.btnDesenhar);
+			this.Controls.Add(this.picture);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox picture;
+		private System.Windows.Forms.Button btnDesenhar;
+	}
+}
+
---
+++ C#/Udemy/CSharp/GDIPlus/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDIPlus
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnDesenhar_Click(object sender, EventArgs e)
+		{
+			//Folha em branco
+			Bitmap folha = new Bitmap(picture.Width, picture.Height); //cria-se o objeto que gerará a imagem
+
+			//Criar desenhador
+			Graphics desenhador = Graphics.FromImage(folha);  //cria-se o esenhador para a folha
+
+			//Desenhar a folha
+			desenhador.Clear(Color.White);  //utiliza o desenhador com o metodo clear para pintar a folha
+
+			#region Linhas
+			Brush pincel1 = new SolidBrush(Color.Red);
+			Pen lapis1 = new Pen(pincel1, 5);
+			Point ponto1 = new Point(100, 200);
+			Point ponto2 = new Point(400, 10);
+
+			Pen lapis2 = new Pen(Color.Black, 10);
+
+			Point ponto3 = new Point(0, 0);
+			Point ponto4 = new Point(100, 200);
+
+			//desenhador.DrawLine(lapis1, ponto1, ponto2 );
+			//desenhador.DrawLine(lapis2, ponto3, ponto4);
+
+			Point[] pontos =
+			{
+				new Point(50,50),
+				new Point(250,50),
+				new Point(250,150),
+				new Point(100,80),
+				new Point(85,140),
+			};
+
+			desenhador.DrawLines(lapis1, pontos);
+			#endregion
+
+			//Utilizar num picture Box
+			picture.BackgroundImage = folha;  
+
+			//Salvar desenho num arquivo
+			folha.Save(@"C:\Users\40357200896\Documents\Gilberto\desenho.jpg", System.Drawing.Imaging.ImageFormat.Jpeg); //salvar em formato de arquivo
+		}
+	}
+} 
