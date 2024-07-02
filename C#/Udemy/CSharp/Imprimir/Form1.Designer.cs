216d776aa6e5d871bdfd7943336bac23b66beeb3
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-07-02 11:57:03 -0300

    trabalhando com impressão

---
+++ C#/Udemy/CSharp/Imprimir/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Imprimir
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+			this.btnImprimir = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// printDocument1
+			// 
+			this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+			// 
+			// btnImprimir
+			// 
+			this.btnImprimir.Location = new System.Drawing.Point(481, 387);
+			this.btnImprimir.Name = "btnImprimir";
+			this.btnImprimir.Size = new System.Drawing.Size(187, 51);
+			this.btnImprimir.TabIndex = 0;
+			this.btnImprimir.Text = "Imprimir";
+			this.btnImprimir.UseVisualStyleBackColor = true;
+			this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(680, 450);
+			this.Controls.Add(this.btnImprimir);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Drawing.Printing.PrintDocument printDocument1;
+		private System.Windows.Forms.Button btnImprimir;
+	}
+}
+
---
+++ C#/Udemy/CSharp/Imprimir/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Imprimir
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnImprimir_Click(object sender, EventArgs e)
+		{
+			printDocument1.Print();
+		}
+
+		private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+		{
+			#region Parte 1
+
+			//string txt = "Trabalhando com impressão.";
+			//Font letra = new Font("Arial", 20, FontStyle.Bold, GraphicsUnit.Point);
+			//Brush pincel = new SolidBrush(Color.Black);
+			//Point pontoInicial = new Point(100, 50);
+
+			////Desenhar o documento para ser impresso
+			//e.Graphics.DrawString(txt, letra, pincel, pontoInicial);
+			//e.Graphics.DrawString("Gilberto", letra, Brushes.Blue, new Point(100, 150));
+			//e.Graphics.FillRectangle(Brushes.Red, new Rectangle(100, 200, 350, 100));
+
+			#endregion
+
+			#region Parte 2
+
+			int x = printDocument1.DefaultPageSettings.Bounds.X;
+			int y = printDocument1.DefaultPageSettings.Bounds.Y;
+			int altura = printDocument1.DefaultPageSettings.Bounds.Height;
+			int largura = printDocument1.DefaultPageSettings.Bounds.Width;
+
+			string titulo = "Titulo da Página";
+			Font letraTitulo = new Font("Verdana", 24, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+			Brush pincelTitulo = new SolidBrush(Color.Red);
+			Rectangle areaTitulo = new Rectangle(x + 50, y + 100, largura - 100, altura - 100);
+			StringFormat formatoTitulo = new StringFormat();
+			formatoTitulo.Alignment = StringAlignment.Center;
+			formatoTitulo.LineAlignment = StringAlignment.Center;
+
+			string txt = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+			Font letra = new Font("Arial", 18, FontStyle.Bold, GraphicsUnit.Point);
+			Brush pincel = new SolidBrush(Color.Black);
+			Rectangle areaTexto = new Rectangle(x + 50, y + 200, largura - 100, altura - 400);
+
+			//Desenhar o documento para ser impresso
+			e.Graphics.DrawString(titulo, letraTitulo, pincelTitulo, areaTitulo, formatoTitulo);
+			e.Graphics.DrawString(txt, letra, pincel, areaTexto);
+			e.Graphics.DrawRectangle(new Pen(Color.Red, 5), areaTitulo);
+			e.Graphics.DrawRectangle(new Pen(Color.Red, 5), areaTexto);
+
+			#endregion
+		}
+	}
+}
