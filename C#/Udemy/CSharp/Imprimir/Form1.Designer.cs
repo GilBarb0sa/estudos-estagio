42495e59b0da918d87183fc0d5facf47f2456176
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-07-03 10:13:53 -0300

    finish trabalhando com impressão

--- C#/Udemy/CSharp/Imprimir/Form1.Designer.cs
+++ C#/Udemy/CSharp/Imprimir/Form1.Designer.cs
@@ -28,8 +28,16 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.printDocument1 = new System.Drawing.Printing.PrintDocument();
 			this.btnImprimir = new System.Windows.Forms.Button();
+			this.printPreviewControl1 = new System.Windows.Forms.PrintPreviewControl();
+			this.btnVisualizar = new System.Windows.Forms.Button();
+			this.btnAnterior = new System.Windows.Forms.Button();
+			this.btnProximo = new System.Windows.Forms.Button();
+			this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+			this.btnVisualizarPadrao = new System.Windows.Forms.Button();
+			this.printDialog1 = new System.Windows.Forms.PrintDialog();
 			this.SuspendLayout();
 			// 
 			// printDocument1
@@ -46,11 +54,77 @@
 			this.btnImprimir.UseVisualStyleBackColor = true;
 			this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
 			// 
+			// printPreviewControl1
+			// 
+			this.printPreviewControl1.Location = new System.Drawing.Point(13, 13);
+			this.printPreviewControl1.Name = "printPreviewControl1";
+			this.printPreviewControl1.Size = new System.Drawing.Size(462, 425);
+			this.printPreviewControl1.TabIndex = 1;
+			// 
+			// btnVisualizar
+			// 
+			this.btnVisualizar.Location = new System.Drawing.Point(481, 13);
+			this.btnVisualizar.Name = "btnVisualizar";
+			this.btnVisualizar.Size = new System.Drawing.Size(187, 51);
+			this.btnVisualizar.TabIndex = 2;
+			this.btnVisualizar.Text = "Visualizar";
+			this.btnVisualizar.UseVisualStyleBackColor = true;
+			this.btnVisualizar.Click += new System.EventHandler(this.btnVisualizar_Click);
+			// 
+			// btnAnterior
+			// 
+			this.btnAnterior.Location = new System.Drawing.Point(481, 70);
+			this.btnAnterior.Name = "btnAnterior";
+			this.btnAnterior.Size = new System.Drawing.Size(85, 51);
+			this.btnAnterior.TabIndex = 3;
+			this.btnAnterior.Text = "Anterior";
+			this.btnAnterior.UseVisualStyleBackColor = true;
+			this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+			// 
+			// btnProximo
+			// 
+			this.btnProximo.Location = new System.Drawing.Point(583, 70);
+			this.btnProximo.Name = "btnProximo";
+			this.btnProximo.Size = new System.Drawing.Size(85, 51);
+			this.btnProximo.TabIndex = 4;
+			this.btnProximo.Text = "Proximo";
+			this.btnProximo.UseVisualStyleBackColor = true;
+			this.btnProximo.Click += new System.EventHandler(this.btnProximo_Click);
+			// 
+			// printPreviewDialog1
+			// 
+			this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+			this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+			this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+			this.printPreviewDialog1.Enabled = true;
+			this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+			this.printPreviewDialog1.Name = "printPreviewDialog1";
+			this.printPreviewDialog1.Visible = false;
+			// 
+			// btnVisualizarPadrao
+			// 
+			this.btnVisualizarPadrao.Location = new System.Drawing.Point(481, 330);
+			this.btnVisualizarPadrao.Name = "btnVisualizarPadrao";
+			this.btnVisualizarPadrao.Size = new System.Drawing.Size(187, 51);
+			this.btnVisualizarPadrao.TabIndex = 5;
+			this.btnVisualizarPadrao.Text = "Visualizar Padrão";
+			this.btnVisualizarPadrao.UseVisualStyleBackColor = true;
+			this.btnVisualizarPadrao.Click += new System.EventHandler(this.btnVisualizarPadrao_Click);
+			// 
+			// printDialog1
+			// 
+			this.printDialog1.UseEXDialog = true;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(680, 450);
+			this.Controls.Add(this.btnVisualizarPadrao);
+			this.Controls.Add(this.btnProximo);
+			this.Controls.Add(this.btnAnterior);
+			this.Controls.Add(this.btnVisualizar);
+			this.Controls.Add(this.printPreviewControl1);
 			this.Controls.Add(this.btnImprimir);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -62,6 +136,13 @@
 
 		private System.Drawing.Printing.PrintDocument printDocument1;
 		private System.Windows.Forms.Button btnImprimir;
+		private System.Windows.Forms.PrintPreviewControl printPreviewControl1;
+		private System.Windows.Forms.Button btnVisualizar;
+		private System.Windows.Forms.Button btnProximo;
+		private System.Windows.Forms.Button btnAnterior;
+		private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+		private System.Windows.Forms.Button btnVisualizarPadrao;
+		private System.Windows.Forms.PrintDialog printDialog1;
 	}
 }
 
--- C#/Udemy/CSharp/Imprimir/Form1.cs
+++ C#/Udemy/CSharp/Imprimir/Form1.cs
@@ -1,18 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Imprimir
 {
 	public partial class Form1 : Form
 	{
+		int x;
+		int y;
+		int largura;
+		int altura;
+		int num_linhas;
+		int pagina;
+		int num_paginas;
+
 		public Form1()
 		{
 			InitializeComponent();
+			pagina = 0;
+			num_paginas = 0;
 		}
 
 		private void btnImprimir_Click(object sender, EventArgs e)
 		{
+			altura = printDocument1.DefaultPageSettings.Bounds.Height;
+			largura = printDocument1.DefaultPageSettings.Bounds.Width;
+			x = 50;
+			y = 50;
+			num_linhas = 0;
+
+			printDialog1.Document = printDocument1;
+			if (printDialog1.ShowDialog() != DialogResult.Cancel)  //Escolha qual impressora será impressa
+			{
+				altura = printDocument1.DefaultPageSettings.Bounds.Height;
+				largura = printDocument1.DefaultPageSettings.Bounds.Width;
+
+				printDocument1.PrinterSettings = printDialog1.PrinterSettings;
+				printDocument1.Print();
+			}
+
 			printDocument1.Print();
 		}
 
@@ -34,31 +62,235 @@ namespace Imprimir
 
 			#region Parte 2
 
-			int x = printDocument1.DefaultPageSettings.Bounds.X;
-			int y = printDocument1.DefaultPageSettings.Bounds.Y;
-			int altura = printDocument1.DefaultPageSettings.Bounds.Height;
-			int largura = printDocument1.DefaultPageSettings.Bounds.Width;
+			//int x = printDocument1.DefaultPageSettings.Bounds.X;
+			//int y = printDocument1.DefaultPageSettings.Bounds.Y;
+			//int altura = printDocument1.DefaultPageSettings.Bounds.Height;
+			//int largura = printDocument1.DefaultPageSettings.Bounds.Width;
+
+			//string titulo = "Titulo da Página";
+			//Font letraTitulo = new Font("Verdana", 24, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+			//Brush pincelTitulo = new SolidBrush(Color.Red);
+			//Rectangle areaTitulo = new Rectangle(x + 50, y + 100, largura - 100, altura - 100);
+			//StringFormat formatoTitulo = new StringFormat();
+			//formatoTitulo.Alignment = StringAlignment.Center;
+			//formatoTitulo.LineAlignment = StringAlignment.Center;
+
+			//string txt = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+			//Font letra = new Font("Arial", 18, FontStyle.Bold, GraphicsUnit.Point);
+			//Brush pincel = new SolidBrush(Color.Black);
+			//Rectangle areaTexto = new Rectangle(x + 50, y + 200, largura - 100, altura - 400);
+
+			////Desenhar o documento para ser impresso
+			//e.Graphics.DrawString(titulo, letraTitulo, pincelTitulo, areaTitulo, formatoTitulo);
+			//e.Graphics.DrawString(txt, letra, pincel, areaTexto);
+			//e.Graphics.DrawRectangle(new Pen(Color.Red, 5), areaTitulo);
+			//e.Graphics.DrawRectangle(new Pen(Color.Red, 5), areaTexto);
+
+			#endregion
+
+			#region Parte 3
+
+			List<string> linhas = new List<string>()
+			{
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"11. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"1. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"2. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"3. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"4. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"5. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"6. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"7. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"8. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"9. Lorem ipson dolor sit amet consectetur dipisicing elit.",
+				"10. Lorem ipson dolor sit amet consectetur dipisicing elit."				
+			};
 
 			string titulo = "Titulo da Página";
 			Font letraTitulo = new Font("Verdana", 24, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
 			Brush pincelTitulo = new SolidBrush(Color.Red);
-			Rectangle areaTitulo = new Rectangle(x + 50, y + 100, largura - 100, altura - 100);
+			Rectangle areaTitulo = new Rectangle(x, y, largura - 100, 100);
 			StringFormat formatoTitulo = new StringFormat();
 			formatoTitulo.Alignment = StringAlignment.Center;
 			formatoTitulo.LineAlignment = StringAlignment.Center;
 
-			string txt = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+			////Desenhar o documento para ser impresso
+			//e.Graphics.DrawString(txt, letra, pincel, areaTexto);
+
 			Font letra = new Font("Arial", 18, FontStyle.Bold, GraphicsUnit.Point);
 			Brush pincel = new SolidBrush(Color.Black);
-			Rectangle areaTexto = new Rectangle(x + 50, y + 200, largura - 100, altura - 400);
 
-			//Desenhar o documento para ser impresso
-			e.Graphics.DrawString(titulo, letraTitulo, pincelTitulo, areaTitulo, formatoTitulo);
-			e.Graphics.DrawString(txt, letra, pincel, areaTexto);
-			e.Graphics.DrawRectangle(new Pen(Color.Red, 5), areaTitulo);
-			e.Graphics.DrawRectangle(new Pen(Color.Red, 5), areaTexto);
+			while (num_linhas < linhas.Count)
+			{
+				if(num_linhas == 00)
+				{
+					e.Graphics.DrawString(titulo, letraTitulo, pincelTitulo, areaTitulo, formatoTitulo);
+					y += 150;
+				}
+
+				e.Graphics.DrawString(linhas[num_linhas], letra, pincel, new Point(x,y));
+				y += 30; //colocando um distanciamento de uma linha para a outra
+				num_linhas++;
+
+				if (y >= altura -50)  //Este if é responsavel por imprimir novas paginas se tiver novas paginas
+				{
+					// Mudar de página
+					y = 50;
+					e.HasMorePages = true;  
+					num_paginas++;
+					break;
+				}
+			}
 
 			#endregion
+		}
+
+		private void btnVisualizar_Click(object sender, EventArgs e)  //Pré visualização do documento
+		{
+			altura = printDocument1.DefaultPageSettings.Bounds.Height;
+			largura = printDocument1.DefaultPageSettings.Bounds.Width;
+			x = 50;
+			y = 50;
+			num_linhas = 0;
+
+			printPreviewControl1.Document = printDocument1;
+		}
+
+		private void btnAnterior_Click(object sender, EventArgs e)  //Visualizar a pagina anterior
+		{
+			if (pagina > 0)
+			{
+				printPreviewControl1.StartPage = --pagina;
+			}
+		}
+
+		private void btnProximo_Click(object sender, EventArgs e) //Visualizar a pagina seguinte
+		{
+			if (pagina < num_paginas)
+			{
+				printPreviewControl1.StartPage = ++pagina;
+			}
+		}
+
+		private void btnVisualizarPadrao_Click(object sender, EventArgs e)
+		{
+			altura = printDocument1.DefaultPageSettings.Bounds.Height;
+			largura = printDocument1.DefaultPageSettings.Bounds.Width;
+			x = 50;
+			y = 50;
+			num_linhas = 0;
+
+			printPreviewDialog1.Document = printDocument1;
+			printPreviewDialog1.ShowDialog();
 		}
 	}
 }
