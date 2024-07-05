08455ad7c1041efe034b882de0f75e50b491fac3
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-07-05 12:00:14 -0300

    estudos referente a primeira semana de julho

---
+++ C#/Udemy/CSharp/SocketCliente/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketCliente
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			try
+			{
+				IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);  //O servidor que o cliente irá acessar
+			
+				socket.Connect(endPoint);
+
+                Console.WriteLine("Conectando com exito..");
+                Console.WriteLine("Insira a informação para enviar: ");
+                Console.WriteLine();
+
+				string info = Console.ReadLine();
+
+				byte[] infoEnviar = Encoding.Default.GetBytes(info);
+
+				socket.Send(infoEnviar, 0, infoEnviar.Length, SocketFlags.None);
+            }
+			catch (Exception)
+			{
+                Console.WriteLine("Não foi possível conectar ao servidor...");
+            }
+
+			socket.Close();
+
+            Console.WriteLine("presione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+	}
+}
---
+++ C#/Udemy/CSharp/SocketServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+			IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);  //O servidor que o cliente irá acessar
+
+			socket.Bind(endPoint);
+
+			socket.Listen(5);
+
+			Console.WriteLine("Escutando...");
+
+			Socket escutar = socket.Accept();  //Retorna uma nova requisição, criando uma conexão com o socket
+
+			byte[] bytes = new byte[255];
+
+			int tamanho = escutar.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+
+			Array.Resize(ref bytes,tamanho);
+
+			Console.WriteLine("Cliente falou: ");
+            Console.WriteLine(Encoding.Default.GetString(bytes));
+
+			socket.Close();
+
+            Console.WriteLine("Servidor Finalizado \n Pressione qualquer tecla para finalizar...");
+			Console.ReadKey();
+
+        }
+	}
+}
