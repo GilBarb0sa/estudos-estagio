201dc1515c69cd92c98a3e98a9284d31d8b4e23d
Gilberto Barbosa da Silva Filho <gilbarbosafilho@gmail.com>
2024-07-26 10:47:13 -0300

    programacao assincrona

---
+++ C#/Udemy/CSharp/ProjectAsync/Exemplo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAsync
+{
+	internal class Exemplo
+	{
+		//método com retorno
+		bool Task_TResult()
+		{
+			return DateTime.IsLeapYear(DateTime.Now.Year);		
+		}
+
+		//método com retorno void
+		void Task_Void()
+		{
+			Task.Delay(2000);
+			Form1.lstRes.Items.Add("Tarefa longa executada");
+		}
+
+		public void Task_LongaDuracao()
+		{
+			bool isAnoBissexto = Task_TResult();
+
+			Form1.lstRes.Items.Add($"{DateTime.Now.Year} {(isAnoBissexto ? " é " : " não é ")} um Ano Bissexto");
+			Task_Void();	
+		}
+	}
+}
---
+++ C#/Udemy/CSharp/ProjectAsync/ExemploAsync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAsync
+{
+	internal class ExemploAsync
+	{
+		//método Task com retorno Task<TResult>
+		async Task<bool> Task_TResult_Async()
+		{
+			return await Task.FromResult<bool>(DateTime.IsLeapYear(DateTime.Now.Year));
+		}
+
+		//método com retorno void (Task)
+		async Task Task_Void_Async()
+		{
+			await Task.Delay(2000);
+			Form1.lstRes.Items.Add("Tarefa longa executada");
+		}
+
+		public async Task Task_LongaDuracao()
+		{
+			bool isAnoBissexto =  await Task_TResult_Async();
+
+			Form1.lstRes.Items.Add($"{DateTime.Now.Year} {(isAnoBissexto ? " é " : " não é ")} um Ano Bissexto");
+			await Task_Void_Async();
+		}
+	}
+}
